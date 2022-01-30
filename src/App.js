ae218b9c8a06f01979f09fac615d83c788cda7f6
sarahabedelrazeq <sarah.abed.elrazeq@gmail.com>
2022-01-30 19:45:36 +0200

    start building the website by structure the component with each other and add image folder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-import './components';
+import React from 'react';
+import Header from './components/Header';
+import SideBar from './components/SideBar';
+import AdList from './components/AdList';
+import Search from './components/Search';
+import {discover, categories} from './data/data';
 
-function App() { 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a 
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App() {
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropdown_btn') && !event.target.matches('.over_li')) {
+      var dropdowns = document.getElementsByClassName("overlay");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i]; 
+        openDropdown.style.display = "none";
+      }
+    }
+  }
+  function discoverShow() {
+    const show = discover.map((x,i) => {
+      return (
+        <AdList key={i} title={x.name} />
+      );
+    });
+    return show;
+  }
+
+  return ( 
+    <div>
+      <Header />
+      <div className='ad_bord'>
+        <img src='./active_ad.png' />
+      </div>
+     <div className='container flex'>
+       <div className='phone_hed'>
+         <SideBar items={discover} title="Discover" /> 
+       </div>
+       <div></div>
+     </div>
+     {discoverShow()}
     </div>
   );
 }
 
 export default App;
+
