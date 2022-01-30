5fdd5b1d694134248a4b3eb333c9a6b14f44b44a
sarahabedelrazeq <sarah.abed.elrazeq@gmail.com>
2022-01-30 19:28:18 +0200

    SecondaryNav component

---
+++ src/components/SecondaryNav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import DropdownListOfList from './DropdownListOfList';
+import LinksBar from './LinksBar';
+import Search from './Search';
+import {categories} from '../data/data';
+
+
+
+function SecondaryNav() {
+    return (
+    <div className='secondary_nav phone_hed'>
+        <div className='content'> 
+            <LinksBar items={[
+            <DropdownListOfList title="Browse Categories" items={categories} />,
+            {value: "What's New",url:"#" },{value: "Trending",url:"#" },{value: "For You",url:"#" }]}/>
+            <Search />
+        </div> 
+    </div>
+    );
+}
+
+export default SecondaryNav;
+
+/*
+
+*/
