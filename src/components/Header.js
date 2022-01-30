5660d5400d02c9be7220f722d5e83aa9decafebe
sarahabedelrazeq <sarah.abed.elrazeq@gmail.com>
2022-01-30 19:30:20 +0200

    add Header to contain PrimaryNav & SecondaryNav components

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PrimaryNav from './PrimaryNav';
+import SecondaryNav from './SecondaryNav';
+
+function Header() {
+
+  return (
+    <header>
+        <PrimaryNav />
+        <SecondaryNav />
+    </header>
+  );
+}
+
+export default Header;
